1808dc3445e2cabf8d9c07db87393eb3d0523e09
Ayush <ay.chaudhary@jacobs-university.de>
2020-01-13 15:29:15 +0100

    Code explanation

--- code/source/Kd_tree.h
+++ code/source/Kd_tree.h
@@ -1,13 +1,14 @@
 #ifndef KDTREE
 #define KDTREE
 
+/*Point class to describe and operate on Cloud Points*/
 class Point{
 	public:
 		Point* left;
 		Point*right;
 		double *co_ordinates;
 		int d;
-		Point(const Point& p){
+		Point(const Point& p){ //copy constructor
 			this->d = p.d;
 			this->left= p.left ;
 			this->right = p.right;
@@ -25,24 +26,24 @@ class Point{
 			left = nullptr;
 			right = nullptr;
 		}
-		friend double cartesian_dist(const Point* p1, const Point& p2);
-		friend std::ostream& operator<<(std::ostream& os, const Point& p);
+		friend double cartesian_dist(const Point* p1, const Point& p2); // calculates cartesian distance
+		friend std::ostream& operator<<(std::ostream& os, const Point& p); // overloading <<
     
 };
 
 
-
+/*Implementation of kd_tree*/
 class Kd_implementation{
     private:
-        int dim;
-        size_t tree_size;
-        Point* l;
-		double val;
-		Point* val_ptr;
-		Point* root;
+        int dim; // dimension of the tree
+        size_t tree_size; // size of the tree
+        Point* l; // list of all the points in the tree
+		double val; // stores the distance of the nearest neighbor of the last neighbor call by the user
+		Point* val_ptr; // stores the nearest neighbor of the last neighbor call
+		Point* root;  // root of the tree
         typedef struct Comp{
             int index;
-			Comp(int i):index(i){
+			Comp(int i):index(i){ // struct Comparator to compare the indices depending upon which axis it is
 				
 				
 				}
@@ -53,15 +54,15 @@ class Kd_implementation{
     
         }C;
         
-        Point *kd_tree(int start, int end, int s_axis);
-		Point nearest_neighbor(const Point& p, int s_axis, Point* p1);
-        void Reset();
+        Point *kd_tree(int start, int end, int s_axis); // creates the tree
+		Point nearest_neighbor(const Point& p, int s_axis, Point* p1); // finds the nearest neighbor
+        void Reset(); // resets val and val_ptr
         
     public:
-       Kd_implementation(Point* p, size_t size, int dim=3);
-	   Point find_nearest(const Point& p, int s_axis, Point* p1);
-	   void Print(Point *p);
-	   Point* getRoot() const{
+       Kd_implementation(Point* p, size_t size, int dim=3); // initializes the class
+	   Point find_nearest(const Point& p, int s_axis, Point* p1); // resets val and calls the nearest neighbor method
+	   void Print(Point *p); //prints the tree
+	   Point* getRoot() const{ 
 		   return root;
 		   }
       ~Kd_implementation(){
